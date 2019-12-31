247043230fe8f4d2e1cdf0c53b0856caf92c179c
Jeff Wu <wuthefwasthat@gmail.com>
2020-01-01 01:01:03 +0800

    ok time to stop

--- levels/paths.py
+++ levels/paths.py
@@ -1,56 +1,47 @@
-from utils import rotate_alphabet, alphabet
+from utils import rotate_alphabet, alphabet, is_alphabet
 
 def _reduce_path(path):
     if not len(path):
-        return []
+        return ''
     segments = []
     cur_dir = None
     last = None
     segment = []
-    for char in path:
+    for cur in path:
         if last is not None:
-            new_dir = ord(char.lower()) - ord(last.lower())
-            if abs(new_dir) > 1:
-                segments.append((segment + [last], cur_dir or 0))
+            new_dir = ord(cur.lower()) - ord(last.lower())
+            if abs(new_dir) > 1:  # jump
                 sign = 1 if new_dir > 0 else -1
-                segments.append(([
-                    rotate_alphabet(last, sign),
-                    rotate_alphabet(char, -sign)
-                ], 'Jump'))
+                segments.append((segment + [last], cur_dir or 0))
+                segments.append(([rotate_alphabet(last, sign), rotate_alphabet(cur, -sign)], 'Jump'))
                 segment = []
                 new_dir = None
-            elif cur_dir is not None and new_dir != cur_dir:
-                if new_dir == 0:
-                    segments.append((segment, cur_dir))
-                    segment = [last]
-                elif cur_dir == 0:
-                    segments.append((segment + [last], cur_dir))
-                    segment = []
-                else:  # V or ^ shape
-                    segments.append((segment, cur_dir))
-                    segments.append(([last], 0))
-                    segment = []
-            else:
+            elif cur_dir is None or new_dir == cur_dir:  # continue
                 segment.append(last)
+            elif new_dir == 0:  # sloped to flat
+                segments.append((segment, cur_dir))
+                segment = [last]
+            elif cur_dir == 0:  # go from flat to sloped
+                segments.append((segment + [last], cur_dir))
+                segment = []
+            else:  # V or ^ shape
+                segments.append((segment, cur_dir))
+                segments.append(([last], 0))
+                segment = []
             cur_dir = new_dir
-        last = char
+        last = cur
     assert last is not None
     segments.append((segment + [last], cur_dir or 0))
 
     result = []
     # print('segments', segments)
     for i, (segment, dir) in enumerate(segments):
-        assert dir in [-1, 1, 0, 'Jump']
+        assert dir in [-1, 1, 0, 'Jump'], dir
         if dir == 0:
-            if i > 0 and i < len(segments) - 1 and segments[i-1][1] == segments[i+1][1] and segments[i-1][1] != 'Jump':
+            if i > 0 and i < len(segments) - 1 and segments[i-1][1] == segments[i+1][1] and segments[i-1][1] != 'Jump':  # 1,0,1 or -1,0,-1 pattern
                 segment = segment[1:]
         if dir in [-1, 1]:
-            # print('i', i, segments)
-            if i > 0 and segments[i-1][1] == 'Jump':
-                pass
-            elif i < len(segments) - 1 and segments[i+1][1] == 'Jump':
-                pass
-            else:
+            if (i == 0 or segments[i-1][1] != 'Jump') and (i == len(segments) - 1 or segments[i+1][1] != 'Jump'):  # chain breaks
                 if i == 0:
                     result.append(segment[0])
                 if i == len(segments) - 1:
@@ -58,31 +49,24 @@ def _reduce_path(path):
                 segment = []
         result.extend(segment)
         # print('segment', segment, 'new result', result)
-
-    # print('result', result)
-    return result
+    # print('final result', result)
+    return ''.join(result)
 
 def paths(x):
     """Jumps in letters reach for each other, consecutive chains break."""
-    result = []
+    result = ''
     curpath = []
     for char in x:
-        if char.lower() not in alphabet:
-            result.extend(_reduce_path(curpath))
+        if not is_alphabet(char):
+            result += _reduce_path(curpath) + char
             curpath = []
-            result.append(char)
         else:
             curpath.append(char)
-    result.extend(_reduce_path(curpath))
-    return ''.join(result)
-
-"""
-TODO: a terrible programmer wrote this whole file, there are probably bugs
-"""
+    result += _reduce_path(curpath)
+    return result
 
 def test_reduce_path(s, expected):
-    actual = ''.join(_reduce_path(list(s)))
-    assert actual == expected, f'{actual} != {expected}'
+    assert _reduce_path(s) == expected, f'{_reduce_path(s)} != {expected}'
 
 test_reduce_path('', '')
 test_reduce_path('m', 'm')
