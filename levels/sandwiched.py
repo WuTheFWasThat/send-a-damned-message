01871b1fce50b060af78dc50700b8fee0904525a
Jeff Wu <wuthefwasthat@gmail.com>
2020-01-03 00:52:00 -0800

    new version of cut

--- levels/sandwiched.py
+++ levels/sandwiched.py
@@ -1,16 +1,48 @@
 from utils import alphabet
 
 def cut_sandwiched(x):
-    pieces = [x]
-    for letter in reversed(alphabet):
-        new_pieces = []
-        for piece in pieces:
-            indices = [i for i, l in enumerate(piece) if l == letter]
-            if len(indices) >= 2:
-                first, last = indices[0], indices[-1]
-                new_pieces.append(piece[:first])
-                new_pieces.append(piece[last + 1:])
-            else:
-                new_pieces.append(piece)
-        pieces = new_pieces
-    return ''.join(pieces)
+    n = len(x)
+    pairs = []
+    for i in range(n):
+        for j in range(i + 1, n):
+            if x[i] == x[j]:
+                pairs.append((i, j))
+
+    is_inner = [True for _ in pairs]
+    for pi, (i1, j1) in enumerate(pairs):
+        # make sure no endpoint is contained within i1
+        for (i2, j2) in pairs:
+            if (i2 > i1 and i2 < j1) or (j2 > i1 and j2 < j1):
+                is_inner[pi] = False
+                break
+
+    deleted = [False for _ in range(n)]
+    for v, (i, j) in zip(is_inner, pairs):
+        if v:
+            for k in range(i, j+1):
+                deleted[k] = True
+    return ''.join([l for k, l in enumerate(x) if not deleted[k]])
+
+assert cut_sandwiched('abba') == 'aa'
+assert cut_sandwiched('baab') == 'bb'
+assert cut_sandwiched('abab') == 'abab'
+assert cut_sandwiched('sand') == 'sand'
+assert cut_sandwiched('sanadweeb') == 'sdwb'
+assert cut_sandwiched('sanadweneb') == 'sanadweneb'
+assert cut_sandwiched('sanadnweeb') == 'sanadnwb'
+
+# OLD VERSION
+# def cut_sandwiched(x):
+#    pieces = [x]
+#     for letter in reversed(alphabet):
+#         new_pieces = []
+#         for piece in pieces:
+#             indices = [i for i, l in enumerate(piece) if l == letter]
+#             if len(indices) >= 2:
+#                 first, last = indices[0], indices[-1]
+#                 new_pieces.append(piece[:first])
+#                 new_pieces.append(piece[last + 1:])
+#             else:
+#                 new_pieces.append(piece)
+#         pieces = new_pieces
+#     return ''.join(pieces)
